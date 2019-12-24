d4e0c1a19afb39655d13a70e8dc1f41fe96eb2f6
hunanTL <13647493750@163.com>
2019-12-24 17:15:06 +0800

    第一个项目

---
+++ springboot_jpa_crud/src/main/java/com/tanlei/springboot_jpa_crud/SpringbootJpaCrudApplication.java
@@ -0,0 +1,13 @@
+package com.tanlei.springboot_jpa_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJpaCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJpaCrudApplication.class, args);
+    }
+
+}
