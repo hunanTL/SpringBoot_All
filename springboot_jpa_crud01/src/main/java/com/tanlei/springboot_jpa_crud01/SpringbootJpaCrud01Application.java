c193ea753403b426ae74fae165c15c7436529ad8
hunanTL <13647493750@163.com>
2019-12-24 17:17:28 +0800

    第二个项目

---
+++ springboot_jpa_crud01/src/main/java/com/tanlei/springboot_jpa_crud01/SpringbootJpaCrud01Application.java
@@ -0,0 +1,13 @@
+package com.tanlei.springboot_jpa_crud01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJpaCrud01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJpaCrud01Application.class, args);
+    }
+
+}
---
+++ springboot_jpa_crud01/src/test/java/com/tanlei/springboot_jpa_crud01/SpringbootJpaCrud01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanlei.springboot_jpa_crud01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJpaCrud01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
